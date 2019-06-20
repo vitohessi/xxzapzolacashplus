4e0cacae7ab6c5448bbc706bba35fd1bef4677c9
vitohessi <vitohessi@gmail.com>
2019-06-20 09:14:01 +0200

    Key removed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig : {
-      apiKey: "AIzaSyBvWN0Q7m7sQeHtLO80WGKf0Z08a1g-bVU",
-      authDomain: "statut-transfert-cashplus.firebaseapp.com",
-      databaseURL: "https://statut-transfert-cashplus.firebaseio.com",
-      projectId: "statut-transfert-cashplus",
+      apiKey: "",
+      authDomain: "ddddddddddddd.firebaseapp.com",
+      databaseURL: "dddddddddddddddddddddd.firebaseio.com",
+      projectId: "dddddddddddddddddddddddddddddddddddddddddddddddddd",
       storageBucket: "",
       messagingSenderId: "706149613919",
-      appId: "1:706149613919:web:f3ef2e411881fc6c"
+      appId: ""
     }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig : {
-      apiKey: "AIzaSyBvWN0Q7m7sQeHtLO80WGKf0Z08a1g-bVU",
-      authDomain: "statut-transfert-cashplus.firebaseapp.com",
-      databaseURL: "https://statut-transfert-cashplus.firebaseio.com",
-      projectId: "statut-transfert-cashplus",
+      apiKey: "",
+      authDomain: "ddddddddddddd.firebaseapp.com",
+      databaseURL: "dddddddddddddddddddddd.firebaseio.com",
+      projectId: "ddddddddddddddddddddddddd",
       storageBucket: "",
       messagingSenderId: "706149613919",
-      appId: "1:706149613919:web:f3ef2e411881fc6c"
+      appId: ""
     }
 };
 
