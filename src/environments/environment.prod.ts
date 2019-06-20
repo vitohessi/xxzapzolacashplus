190c0be2aa33fd847a815f0429db8b25ea1e4b22
vitohessi <vitohessi@gmail.com>
2019-06-20 09:13:12 +0200

    styles modified

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig : {
-      apiKey: "",
-      authDomain: "ddddddddddddd.firebaseapp.com",
-      databaseURL: "dddddddddddddddddddddd.firebaseio.com",
-      projectId: "ddddddddddddddddddddddddd",
+      apiKey: "AIzaSyBvWN0Q7m7sQeHtLO80WGKf0Z08a1g-bVU",
+      authDomain: "statut-transfert-cashplus.firebaseapp.com",
+      databaseURL: "https://statut-transfert-cashplus.firebaseio.com",
+      projectId: "statut-transfert-cashplus",
       storageBucket: "",
       messagingSenderId: "706149613919",
-      appId: ""
+      appId: "1:706149613919:web:f3ef2e411881fc6c"
     }
 };
